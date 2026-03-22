5b36593670e1285b4e26643bdc2aba427a5b835e
Hazelnoot <acomputerdog@gmail.com>
2026-03-22 00:31:12 -0400

    normalize mentions in extractMentions helper

--- packages/frontend/src/utility/extract-mentions.ts
+++ packages/frontend/src/utility/extract-mentions.ts
@@ -5,14 +5,42 @@
 
 // test is located in test/extract-mentions
 
+import { acct } from 'misskey-js';
+import { toASCII } from 'punycode.js';
 import * as mfm from 'mfm-js';
+import * as config from '@@/js/config.js';
 
 export type MfmMention = mfm.MfmMention['props'];
 
-export function extractMentions(nodes: mfm.MfmNode[]): MfmMention[] {
-	// TODO: 重複を削除
-	const mentionNodes = mfm.extract(nodes, (node) => node.type === 'mention') as mfm.MfmMention[];
-	const mentions = mentionNodes.map(x => x.props);
+const localHost = normalize(config.host);
 
-	return mentions;
+export function extractMentions(nodes: mfm.MfmNode[], selfHost: string | null = null): MfmMention[] {
+	const normalSelfHost = selfHost != null ? normalize(selfHost) : null;
+
+	const mentionNodes = mfm.extract(nodes, (node) => node.type === 'mention') as mfm.MfmMention[];
+	const mentions = mentionNodes.map(({ props: mention }) => {
+		// Normalize hostnames
+		const normalHost = mention.host != null ? normalize(mention.host) : null;
+		if (normalHost == null) {
+			mention.host = normalSelfHost; // replace null with the actual origin
+		} else if (normalHost === localHost) {
+			mention.host = null; // replace local host with null
+		} else {
+			mention.host = normalHost; // preserve other hosts
+		}
+
+		// Normalize the acct form in case of weirdness from MFM
+		mention.acct = acct.toString(mention);
+
+		// Generate a further-normalized key
+		const key = mention.acct.toLowerCase();
+		return [key, mention] as const;
+	});
+
+	// Deduplicate the list, but preserve the as-entered mentions.
+	return Array.from(new Map(mentions).values());
+}
+
+function normalize(host: string): string {
+	return toASCII(host).toLowerCase();
 }
